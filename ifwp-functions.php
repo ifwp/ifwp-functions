b31b716b60edee4c776d40ac69a91f437ad238a0
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-07-13 18:43:32 -0600

    IFWP

--- ifwp-functions.php
+++ ifwp-functions.php
@@ -10,7 +10,7 @@ Network:
 Plugin Name: IFWP Functions
 Plugin URI: https://github.com/ifwp/ifwp-functions
 Text Domain: ifwp-functions
-Version: 2020.7.12
+Version: 2020.7.13.1
 */
 
 if(!defined('ABSPATH')){
