53fb74d7f11fd159b40255de510830fd665d6ee1
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-08-02 15:54:22 -0600

    IFWP

--- functions/themes.php
+++ functions/themes.php
@@ -20,17 +20,64 @@ if(!function_exists('ifwp_add_larger_image_sizes')){
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+if(!function_exists('ifwp_enqueue_bootstrap')){
+    function ifwp_enqueue_bootstrap(){
+        static $already_called = false;
+        if(!$already_called){
+            $already_called = true;
+            add_action('wp_enqueue_scripts', function(){
+                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/css/bootstrap.min.css';
+                wp_enqueue_style('bootstrap', $src, [], '4.5.0');
+                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/js/bootstrap.min.js';
+                wp_enqueue_script('bootstrap', $src, ['jquery'], '4.5.0', true);
+            });
+        }
+    }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ifwp_enqueue_bs_custom_file_input')){
+    function ifwp_enqueue_bs_custom_file_input(){
+        static $already_called = false;
+        if(!$already_called){
+            $already_called = true;
+            add_action('wp_enqueue_scripts', function(){
+                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/js/bs-custom-file-input.min.js';
+                wp_enqueue_script('bs-custom-file-input', $src, ['jquery'], '1.3.4', true);
+            });
+        }
+    }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ifwp_enqueue_popper')){
+    function ifwp_enqueue_popper(){
+        static $already_called = false;
+        if(!$already_called){
+            $already_called = true;
+            add_action('wp_enqueue_scripts', function(){
+				$src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/js/popper.min.js';
+                wp_enqueue_script('popper', $src, ['jquery'], '1.16.1-lts', true);
+            });
+        }
+    }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 if(!function_exists('ifwp_support_floating_labels')){
     function ifwp_support_floating_labels(){
         static $already_called = false;
         if(!$already_called){
             $already_called = true;
             add_action('wp_enqueue_scripts', function(){
-                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/floating-labels.css';
-                $ver = filemtime(plugin_dir_path(IFWP_FUNCTIONS) . 'assets/floating-labels.css');
+                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/css/floating-labels.css';
+                $ver = filemtime(plugin_dir_path(IFWP_FUNCTIONS) . 'assets/css/floating-labels.css');
                 wp_enqueue_style('ifwp-floating-labels', $src, [], $ver);
-                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/floating-labels.js';
-                $ver = filemtime(plugin_dir_path(IFWP_FUNCTIONS) . 'assets/floating-labels.js');
+                $src = plugin_dir_url(IFWP_FUNCTIONS) . 'assets/js/floating-labels.js';
+                $ver = filemtime(plugin_dir_path(IFWP_FUNCTIONS) . 'assets/js/floating-labels.js');
                 wp_enqueue_script('ifwp-floating-labels', $src, ['jquery'], $ver, true);
             });
         }
